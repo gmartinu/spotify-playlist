07934136f007f28382e7d92424b7e5f60c8c84be
Gabriel Martinusso <gabrielbramos@outlook.com>
2021-03-16 06:15:20 -0300

    Initial commit with front-end

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#000000',
+      contrastText: "#FFFFFF"
+    },
+  },
+});
+
+export default theme;
